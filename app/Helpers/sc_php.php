56e4758855a2aec2cb2bcd74f4b7975c836e02d9
Miguel Leon <miguel-leon@aj.com.py>
2021-11-27 05:44:27 -0300

    Corrección a listado de juegos gratis

--- app/Helpers/sc_php.php
+++ app/Helpers/sc_php.php
@@ -429,13 +429,18 @@ function sc_url_buscar_imagenes_google($busqueda){
 }
 
 function sc_url_curl($url) {
-    $ch = curl_init();
-    $timeout = 5;
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-    $data = curl_exec($ch);
-    curl_close($ch);
+    try{
+        $ch = curl_init();
+        $timeout = 5;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $data = curl_exec($ch);
+        curl_close($ch);
+    }catch (Exception $e){
+
+    }
+
     return $data;
 }
 
